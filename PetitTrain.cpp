0d550cc894fd9e9e46fa7294ed63b2786c8eafbd
nicolasrnp@gmail.com <nicolasrnp@gmail.com@bfcabc1c-525b-201b-f307-14d02e53008e>
2011-12-22 19:06:27 +0000

    modification : - préparation de l'attribution d'une trajectoire à une camera (trajectoire libre correspond a la navigation dans la scene habituelle, la trajectoire de la cabine est restreinte la vue de la cabine avec la prise en compte du mouvement du train (fct trigo) sinon la camera ne bouge pas mais le train si donc ...). - ajout de la gestion de la camera sous différente position pas encore    terminé MonModuleCamera - ajout de la manipulation de la scene(rotation) via la souris MonModuleSouris - ajout de variable de compilation pour le debug (debug_camera debug_souris) - par défaut les lumières sont éteintes     - 'l' pour activer la lumiere blanche     - 'm' pour activer les lumières jaunes (spots) positionné au niveau des phares -bidouillage toujours en cours- à l'air d'être bon       2 lumières : une pour chaques phares. ces lumières ont un effet d'atténuation de 0.05f sur chaques couleurs, et chaques param de lumière (diffuse, speculaire) - ajout des couleurs du mode "vieux"

--- PetitTrain.cpp
+++ PetitTrain.cpp
@@ -1498,7 +1498,7 @@ void display(void)
                         depCamX, depCamY, depCamZ, 
                         0.0, 1.0, 0.0
                     );*/
-        lookAt(*cameraCourante);
+        lookAt(cameraCourante);
         
 		glPushMatrix();
     		glColor4fv(couleurRouge());
