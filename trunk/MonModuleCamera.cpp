853eac1379ada48ea564d495a2947ace3b54c200
nicolasrnp@gmail.com <nicolasrnp@gmail.com@bfcabc1c-525b-201b-f307-14d02e53008e>
2011-12-22 19:06:27 +0000

    modification : - préparation de l'attribution d'une trajectoire à une camera (trajectoire libre correspond a la navigation dans la scene habituelle, la trajectoire de la cabine est restreinte la vue de la cabine avec la prise en compte du mouvement du train (fct trigo) sinon la camera ne bouge pas mais le train si donc ...). - ajout de la gestion de la camera sous différente position pas encore    terminé MonModuleCamera - ajout de la manipulation de la scene(rotation) via la souris MonModuleSouris - ajout de variable de compilation pour le debug (debug_camera debug_souris) - par défaut les lumières sont éteintes     - 'l' pour activer la lumiere blanche     - 'm' pour activer les lumières jaunes (spots) positionné au niveau des phares -bidouillage toujours en cours- à l'air d'être bon       2 lumières : une pour chaques phares. ces lumières ont un effet d'atténuation de 0.05f sur chaques couleurs, et chaques param de lumière (diffuse, speculaire) - ajout des couleurs du mode "vieux"

--- trunk/MonModuleCamera.cpp
+++ trunk/MonModuleCamera.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+//void (*trajectoire)(Camera) = NULL;
+
 void initCamera(Camera *cam, GLdouble eyeX, GLdouble eyeY, GLdouble eyeZ, GLdouble centerX, GLdouble centerY, GLdouble centerZ, GLdouble upX, GLdouble upY, GLdouble upZ)
 {
     cam->eyeX = eyeX;
@@ -22,14 +24,14 @@ void setEyeX(Camera *cam, GLdouble x)
     cam->eyeX = x;
 }
 
-void setEyeY(Camera *cam, GLdouble x)
+void setEyeY(Camera *cam, GLdouble y)
 {
-    cam->eyeY = x;
+    cam->eyeY = y;
 }
 
-void setEyeZ(Camera *cam, GLdouble x)
+void setEyeZ(Camera *cam, GLdouble z)
 {
-    cam->eyeZ = x;
+    cam->eyeZ = z;
 }
 
 void setCenterX(Camera *cam, GLdouble x)
@@ -37,14 +39,14 @@ void setCenterX(Camera *cam, GLdouble x)
     cam->centerX = x;
 }
 
-void setCenterY(Camera *cam, GLdouble x)
+void setCenterY(Camera *cam, GLdouble y)
 {
-    cam->centerY = x;
+    cam->centerY = y;
 }
 
-void setCenterZ(Camera *cam, GLdouble x)
+void setCenterZ(Camera *cam, GLdouble z)
 {
-    cam->centerZ = x;
+    cam->centerZ = z;
 }
 
 void setUpX(Camera *cam, GLdouble x)
@@ -52,21 +54,24 @@ void setUpX(Camera *cam, GLdouble x)
     cam->upX = x;
 }
 
-void setUpY(Camera *cam, GLdouble x)
+void setUpY(Camera *cam, GLdouble y)
 {
-    cam->upY = x;
+    cam->upY = y;
 }
 
-void setUpZ(Camera *cam, GLdouble x)
+void setUpZ(Camera *cam, GLdouble z)
 {
-    cam->upZ = x;
+    cam->upZ = z;
 }
 
-void lookAt(Camera cam)
+void lookAt(Camera *cam, void (*trajectoire)(Camera*))
 {
-    gluLookAt(cam.eyeX, cam.eyeY, cam.eyeZ, cam.centerX, cam.centerY, cam.centerZ, cam.upX, cam.upY, cam.upZ);
+    if (trajectoire != NULL)
+        trajectoire(cam);
+    gluLookAt(cam->eyeX, cam->eyeY, cam->eyeZ, cam->centerX, cam->centerY, cam->centerZ, cam->upX, cam->upY, cam->upZ);
+    
 #if defined(debug_camera) && debug_camera == 1
-    afficherCam(cam);
+    afficherCam(*cam);
 #endif
 }
 
--- trunk/MonModuleCamera.h
+++ trunk/MonModuleCamera.h
@@ -6,7 +6,7 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 
-#define debug_camera 1
+#define debug_camera 0
 
 typedef struct camera
 {
@@ -23,22 +23,32 @@ typedef struct camera
     GLdouble upZ;
 }Camera;
 
-void initCamera(Camera *cam, GLdouble eyeX = 0, GLdouble eyeY = 0, GLdouble eyeZ = 0, GLdouble centerX = 0, GLdouble centerY = 0, GLdouble centerZ = 0, GLdouble upX = 0, GLdouble upY = 1, GLdouble upZ = 0);
+//extern void (*trajectoire)(Camera);
+
+void initCamera(Camera *cam, 
+                GLdouble eyeX = 0,
+                GLdouble eyeY = 0, 
+                GLdouble eyeZ = 0, 
+                GLdouble centerX = 0, 
+                GLdouble centerY = 0, 
+                GLdouble centerZ = 0, 
+                GLdouble upX = 0, 
+                GLdouble upY = 1, 
+                GLdouble upZ = 0);
 
 void setEyeX(Camera *cam, GLdouble x);
-void setEyeY(Camera *cam, GLdouble x);
-void setEyeZ(Camera *cam, GLdouble x);
+void setEyeY(Camera *cam, GLdouble y);
+void setEyeZ(Camera *cam, GLdouble z);
 
 void setCenterX(Camera *cam, GLdouble x);
-void setCenterY(Camera *cam, GLdouble x);
-void setCenterZ(Camera *cam, GLdouble x);
+void setCenterY(Camera *cam, GLdouble y);
+void setCenterZ(Camera *cam, GLdouble z);
 
 void setUpX(Camera *cam, GLdouble x);
-void setUpY(Camera *cam, GLdouble x);
-void setUpZ(Camera *cam, GLdouble x);
+void setUpY(Camera *cam, GLdouble y);
+void setUpZ(Camera *cam, GLdouble z);
 
-void lookAt(Camera cam);
-
+void lookAt(Camera *cam, void (*trajectoire)(Camera*) = NULL);
 void afficherCam(Camera cam);
 
 #endif
